03e370995df3051d787cfdc79f90822b6c5a02f4
Stijntje88 <Stijnvandaelen@outlook.com>
2025-09-30 10:00:40 +0200

    create teacher model

---
+++ Data/Teacher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Make_it_stick.Data
+{
+    public class Teacher
+    {
+        public int TeacherId { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public string CurioEmail { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
