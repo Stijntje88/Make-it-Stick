7d91939881459b239b22bcf49f6034e51e5c7e98
Stijntje88 <Stijnvandaelen@outlook.com>
2025-09-30 10:00:54 +0200

    create weekly challenge model

---
+++ Data/WeeklyChallenge.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Make_it_stick.Data
+{
+    public class WeeklyChallenge
+    {
+        public int ChallengeId { get; set; }
+        public int TeacherId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public DateTime Deadline { get; set; }
+        public bool OpenStatus { get; set; }
+    }
+}
