02ee3d51971eb0dcd882ef22477617385999848d
ANKERBOYGame <daanvanyperen@ziggo.nl>
2025-09-30 09:42:11 +0200

    eerste model
    de eerste model is student

---
+++ models/student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Make_it_stick.models
+{
+    internal class student
+    {
+
+    }
+}
